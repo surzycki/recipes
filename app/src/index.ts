cdb97c6d52eb63eaad21ad0f3f248ffbcf627e61
stefan <stefan.surzycki@gmail.com>
2023-09-19 21:33:01 +0200

    [ADD] update red rice

--- app/src/index.ts
+++ app/src/index.ts
@@ -17,8 +17,6 @@ async function main() {
 
   const prompt: string = `Can you give me a paragraph or two summary of what ${processedRecipe.title} is ? make it 30-40 words long`;
   const summary: string | null = await callOpenAiApi(prompt);
-  // const summary: string =
-  //  "Dwaejigogi Bokkeum is a popular Korean dish consisting of stir-fried pork belly. The pork is typically marinated in a soy sauce-based mixture, imbued with flavors of garlic, ginger, and Korean spices. Served with rice and kimchi, this dish offers a well-rounded, spice-balanced meal.";
 
   processedRecipe.summary = summary ?? "";
   generateHtml(processedRecipe);
